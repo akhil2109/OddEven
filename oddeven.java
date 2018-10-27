864b92e544065a1ffadc68a24ea44be7389a17cc
prabal gupta <prabalgupta4343@gmail.com>
2018-10-27 15:59:01 +0530

    added changes oddven.java to .java
    Signed-off-by: prabal gupta <prabalgupta4343@gmail.com>

---
+++ oddeven.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+
+
+class GFG
+{
+     public static int summation(int n)
+     {
+       int some = 0;
+       for (int i = 1; i <= n; i++)
+            some += (i * i);
+
+
+
+            return sum;
+      }
+
+
+      public static void main(string args[])
+      {
+          int n = 2;
+          system.out.println(summation(n));
+      }
+}
